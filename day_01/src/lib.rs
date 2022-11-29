16d3ab5180059c8f4a29edb8e5e5b4cca95edb91
Jan Lek <j.lek@me.com>
2022-11-29 20:43:57 +0000

    Prepare for day 1

---
+++ day_01/src/lib.rs
@@ -0,0 +1,26 @@
+const _: &str = include_str!("input.txt");
+
+pub fn part_1() -> i32 {
+    todo!()
+}
+
+pub fn part_2() -> i32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore = "todo"]
+    fn test_part_1() {
+        assert_eq!(part_1(), 42);
+    }
+
+    #[test]
+    #[ignore = "todo"]
+    fn test_part_2() {
+        assert_eq!(part_2(), 42);
+    }
+}
