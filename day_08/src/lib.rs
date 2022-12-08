583a024317f299cf0e56bca879e06a3c78e52b2b
Jan Lek <j.lek@me.com>
2022-12-08 19:42:35 +0000

    Refactor day 08

--- day_08/src/lib.rs
+++ day_08/src/lib.rs
@@ -4,77 +4,72 @@ use std::ops::Index;
 
 #[must_use]
 pub fn part_1(input: &str) -> usize {
-    let grove = Grove::from(input);
-    (0..grove.num_rows)
-        .flat_map(|row| (0..grove.row_length).map(move |column| (row, column)))
-        .filter(|(row, column)| is_visible(&grove, *row, *column))
-        .count()
+    Grove::from(input).count_visible_trees()
 }
 
 #[must_use]
 pub fn part_2(input: &str) -> usize {
-    let grove = Grove::from(input);
-    (0..grove.num_rows)
-        .flat_map(|row| (0..grove.row_length).map(move |column| (row, column)))
-        .map(|(row, column)| scenic_score(&grove, row, column))
-        .max()
-        .unwrap()
-}
-
-fn is_visible(grove: &Grove, row: usize, column: usize) -> bool {
-    let tree_height = grove[(row, column)];
-    (0..row).all(|r| grove[(r, column)] < tree_height)
-        || (row + 1..grove.num_rows).all(|r| grove[(r, column)] < tree_height)
-        || (0..column).all(|c| grove[(row, c)] < tree_height)
-        || (column + 1..grove.row_length).all(|c| grove[(row, c)] < tree_height)
-}
-
-fn scenic_score(grove: &Grove, row: usize, column: usize) -> usize {
-    let tree_height = grove[(row, column)];
-
-    count_visible_trees(grove, tree_height, (0..row).rev().map(|r| (r, column)))
-        * count_visible_trees(
-            grove,
-            tree_height,
-            (row + 1..grove.num_rows).map(|r| (r, column)),
-        )
-        * count_visible_trees(grove, tree_height, (0..column).rev().map(|c| (row, c)))
-        * count_visible_trees(
-            grove,
-            tree_height,
-            (column + 1..grove.row_length).map(|c| (row, c)),
-        )
-}
-
-fn count_visible_trees(
-    grove: &Grove,
-    tree_height: u8,
-    points: impl Iterator<Item = (usize, usize)>,
-) -> usize {
-    let mut count = 0;
-    for (row, column) in points {
-        count += 1;
-        if grove[(row, column)] >= tree_height {
-            break;
-        }
-    }
-    count
+    Grove::from(input).max_scenic_score()
 }
 
 struct Grove<'a> {
     trees: &'a [u8],
-    row_length: usize,
+    num_columns: usize,
     num_rows: usize,
+}
+
+impl Grove<'_> {
+    fn all_coordinates(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        (0..self.num_rows).flat_map(|row| (0..self.num_columns).map(move |column| (row, column)))
+    }
+
+    fn count_visible_trees(&self) -> usize {
+        self.all_coordinates()
+            .filter(|&(row, column)| {
+                let tree_height = self[(row, column)];
+                // Check in every direction if there are no higher trees blocking the view.
+                (0..row).all(|r| self[(r, column)] < tree_height)
+                    || (row + 1..self.num_rows).all(|r| self[(r, column)] < tree_height)
+                    || (0..column).all(|c| self[(row, c)] < tree_height)
+                    || (column + 1..self.num_columns).all(|c| self[(row, c)] < tree_height)
+            })
+            .count()
+    }
+
+    fn max_scenic_score(&self) -> usize {
+        self.all_coordinates()
+            .map(|(row, column)| {
+                // Count the trees in line of sight in every direction.
+                self.count_in_line_of_sight((0..=row).rev().map(|r| (r, column)))
+                    * self.count_in_line_of_sight((row..self.num_rows).map(|r| (r, column)))
+                    * self.count_in_line_of_sight((0..=column).rev().map(|c| (row, c)))
+                    * self.count_in_line_of_sight((column..self.num_columns).map(|c| (row, c)))
+            })
+            .max()
+            .unwrap()
+    }
+
+    fn count_in_line_of_sight(&self, mut points: impl Iterator<Item = (usize, usize)>) -> usize {
+        let tree_height = self[points.next().unwrap()];
+        let mut count = 0;
+        for (row, column) in points {
+            count += 1;
+            if self[(row, column)] >= tree_height {
+                break;
+            }
+        }
+        count
+    }
 }
 
 impl<'a> From<&'a str> for Grove<'a> {
     fn from(s: &'a str) -> Self {
         let trees = s.as_bytes();
-        let row_length = trees.iter().position(|&byte| byte == b'\n').unwrap();
-        let num_rows = trees.len() / row_length;
+        let num_columns = trees.iter().position(|&byte| byte == b'\n').unwrap();
+        let num_rows = trees.len() / num_columns;
         Self {
             trees,
-            row_length,
+            num_columns,
             num_rows,
         }
     }
@@ -84,7 +79,7 @@ impl Index<(usize, usize)> for Grove<'_> {
     type Output = u8;
 
     fn index(&self, (row, column): (usize, usize)) -> &Self::Output {
-        &self.trees[row * (self.row_length + 1) + column]
+        &self.trees[row * (self.num_columns + 1) + column]
     }
 }
 
