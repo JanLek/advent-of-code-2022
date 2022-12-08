9c5ed009161ebf5906107f091525c733d2acc644
Jan Lek <j.lek@me.com>
2022-12-08 07:19:23 +0000

    Scaffold day 08

---
+++ day_08/benches/bench.rs
@@ -0,0 +1,15 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use day_08::{part_1, part_2};
+
+const INPUT: &str = include_str!("../input.txt");
+
+fn part_1_benchmark(c: &mut Criterion) {
+    c.bench_function("part_1", |b| b.iter(|| part_1(black_box(INPUT))));
+}
+
+fn part_2_benchmark(c: &mut Criterion) {
+    c.bench_function("part_2", |b| b.iter(|| part_2(black_box(INPUT))));
+}
+
+criterion_group!(benches, part_1_benchmark, part_2_benchmark);
+criterion_main!(benches);
