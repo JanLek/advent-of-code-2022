04d524040f300bbc2c696775f6f61b52695bf676
Jan Lek <j.lek@me.com>
2022-12-09 08:11:30 +0000

    Solution for day 09 part 1

--- day_09/src/lib.rs
+++ day_09/src/lib.rs
@@ -1,8 +1,51 @@
 #![deny(clippy::all, clippy::pedantic)]
+#![allow(clippy::missing_panics_doc)]
+
+use std::{
+    cmp::Ordering::{Equal, Greater, Less},
+    collections::HashSet,
+};
 
 #[must_use]
-pub fn part_1(input: &str) -> u32 {
-    todo!()
+pub fn part_1(input: &str) -> usize {
+    let head_motions = input.lines().map(|line| {
+        let (direction, num_steps) = line.split_once(' ').unwrap();
+        (direction, num_steps.parse::<u8>().unwrap())
+    });
+
+    let mut head: (i16, i16) = (0, 0);
+    let mut tail: (i16, i16) = (0, 0);
+    let mut tail_history = HashSet::new();
+    tail_history.insert(tail);
+
+    for (direction, num_steps) in head_motions {
+        for _ in 0..num_steps {
+            match direction {
+                "U" => head.1 += 1,
+                "R" => head.0 += 1,
+                "D" => head.1 -= 1,
+                "L" => head.0 -= 1,
+                _ => panic!(),
+            };
+
+            if (head.0 - tail.0).abs() > 1 || (head.1 - tail.1).abs() > 1 {
+                match head.0.cmp(&tail.0) {
+                    Greater => tail.0 += 1,
+                    Equal => (),
+                    Less => tail.0 -= 1,
+                }
+                match head.1.cmp(&tail.1) {
+                    Greater => tail.1 += 1,
+                    Equal => (),
+                    Less => tail.1 -= 1,
+                }
+
+                tail_history.insert(tail);
+            }
+        }
+    }
+
+    tail_history.len()
 }
 
 #[must_use]
@@ -18,15 +61,13 @@ mod tests {
     const INPUT: &str = include_str!("../input.txt");
 
     #[test]
-    #[ignore = "TODO"]
     fn part_1_sample_test() {
-        assert_eq!(part_1(SAMPLE_INPUT), 0);
+        assert_eq!(part_1(SAMPLE_INPUT), 13);
     }
 
     #[test]
-    #[ignore = "TODO"]
     fn part_1_test() {
-        assert_eq!(part_1(INPUT), 0);
+        assert_eq!(part_1(INPUT), 6_376);
     }
 
     #[test]
